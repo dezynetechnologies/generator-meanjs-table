64a7929ffa1343b46d1fe272d936fb6fa1d02298
Dheeraj Kaushik <dheeraj@dezyne.in>
2015-04-17 09:48:22 +0530

    Added support for bootstrap material design

--- app/templates/config/env/_all.js
+++ app/templates/config/env/_all.js
@@ -15,6 +15,9 @@ module.exports = {
 			css: [
 				'public/lib/bootstrap/dist/css/bootstrap.css',
 				'public/lib/bootstrap/dist/css/bootstrap-theme.css',
+				'public/lib/bootstrap-material-design/dist/css/roboto.min.css'
+				'public/lib/bootstrap-material-design/dist/css/material.min.css'
+				'public/lib/bootstrap-material-design/dist/css/ripples.min.css'
 				'public/lib/ng-table/ng-table.min.css'
 			],
 			js: [
@@ -25,6 +28,9 @@ module.exports = {
 				'public/lib/angular-touch/angular-touch.js', <% } if (angularSanitize) { %>
 				'public/lib/angular-sanitize/angular-sanitize.js', <% } %>
 				'public/lib/angular-ui-router/release/angular-ui-router.js',
+				'//code.jquery.com/jquery-1.10.2.min.js',
+				'public/lib/bootstrap-material-design/dist/js/ripples.min.js',
+				'public/lib/bootstrap-material-design/dist/js/material.min.js',
 				'public/lib/angular-ui-utils/ui-utils.js',
 				'public/lib/angular-bootstrap/ui-bootstrap-tpls.js',
 				'public/lib/ng-table/ng-table.min.js',
